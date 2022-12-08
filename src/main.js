37c5d00a01dcac2a21953e2432eb8c8b8342e7f9
valkiboy <val_kik@mail.ru>
2022-12-08 16:30:16 +0300

    Разбивает разметку на компоненты, заводит презентер

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import ListPresenter from "./presenter/list-presenter";
+import { render } from "./render";
+import { RenderPosition } from "./render";
+import TripFiltersView from "./view/trip-filters";
+
+const siteTripElement = document.querySelector('.trip-events');
+const siteControlsElement = document.querySelector('.trip-controls__filters');
+const listPresenter = new ListPresenter({listContainer: siteTripElement});
+
+render (new TripFiltersView(), siteControlsElement, RenderPosition.BEFOREEND);
+
+listPresenter.init();
---
+++ src/presenter/list-presenter.js
@@ -0,0 +1,27 @@
+import { render } from "../render";
+import { RenderPosition } from "../render";
+import ListView from "../view/list-view";
+import TripSortView from "../view/trip-sort";
+import EventView from "../view/event";
+import NewPointFormView from "../view/new-point-form";
+import EditPointFormView from "../view/edit-point-form";
+
+export default class ListPresenter {
+  listComponent = new ListView();
+
+  constructor({listContainer}) {
+    this.listContainer = listContainer;
+  }
+
+  init() {
+    render(this.listComponent, this.listContainer);
+    render (new TripSortView(), this.listComponent.getElement(), RenderPosition.BEFOREBEGIN);
+    render (new EditPointFormView(), this.listComponent.getElement(), RenderPosition.AFTERBEGIN);
+    render (new NewPointFormView(), this.listComponent.getElement(), RenderPosition.BEFOREEND);
+
+
+    for (let i = 0; i < 3; i++) {
+      render (new EventView(), this.listComponent.getElement(), RenderPosition.BEFOREEND);
+    }
+  }
+}
